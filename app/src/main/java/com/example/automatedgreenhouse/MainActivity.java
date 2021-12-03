ce4913ee7ca535d514a4b045525d5a397a47ca98
scottj98 <44309841+scottj98@users.noreply.github.com>
2021-12-03 14:02:56 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/automatedgreenhouse/MainActivity.java
+++ app/src/main/java/com/example/automatedgreenhouse/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,11 +17,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvSunlight;
     private TextView tvMoisture;
     private TextView tvTemperature;
+    private TextView tvHour;
+    private TextView tvMinute;
+    private TextView tvSecond;
+
+    private EditText edtHour;
+    private EditText edtMinute;
+    private EditText edtSecond;
+
+    private Button btnSchedule;
+    private Button btnCancelSchedule;
 
     private ImageView imgViewMoisture;
     private ImageView imgViewSunlight;
@@ -34,13 +49,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Timer timer = new Timer();
 
         tvSunlight = (TextView) findViewById(R.id.tvSun);
         tvMoisture = (TextView) findViewById(R.id.tvMoist);
         tvTemperature = (TextView) findViewById(R.id.tvTemp);
+        tvHour = (TextView) findViewById(R.id.tvHours);
+        tvMinute = (TextView) findViewById(R.id.tvMins);
+        tvSecond = (TextView) findViewById(R.id.tvSeconds);
+
+        edtHour = (EditText) findViewById(R.id.edtHours);
+        edtMinute = (EditText) findViewById(R.id.edtMins);
+        edtSecond = (EditText) findViewById(R.id.edtSeconds);
+
+        btnSchedule = (Button) findViewById(R.id.btnSet);
+        btnCancelSchedule = (Button) findViewById(R.id.btnCancel);
 
         imgBtnWater = (ImageButton) findViewById(R.id.imgBtnWater);
         imgBtnSchedule = (ImageButton) findViewById(R.id.imgBtnSchedule);
+        
+        tvHour.setVisibility(View.INVISIBLE);
+        tvMinute.setVisibility(View.INVISIBLE);
+        tvSecond.setVisibility(View.INVISIBLE);
+        edtHour.setVisibility(View.INVISIBLE);
+        edtMinute.setVisibility(View.INVISIBLE);
+        edtSecond.setVisibility(View.INVISIBLE);
+        btnSchedule.setVisibility(View.INVISIBLE);
+        btnCancelSchedule.setVisibility(View.INVISIBLE);
 
         // initialize the database reference
          //database = FirebaseDatabase.getInstance();
@@ -88,6 +124,48 @@ public class MainActivity extends AppCompatActivity {
                 scheduleButtonClicked();
             }
         });
+        
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnCancelSchedule.setVisibility(View.VISIBLE);
+
+                String h = edtHour.getText().toString();
+                String m = edtMinute.getText().toString();
+                String s = edtSecond.getText().toString();
+
+                int hours = Integer.parseInt(h);
+                int minutes = Integer.parseInt(m);
+                int seconds = Integer.parseInt(s);
+
+                int interval = (3600000 * hours) + (60000 * minutes) + (1000 * seconds);
+
+                tvHour.setVisibility(View.INVISIBLE);
+                tvMinute.setVisibility(View.INVISIBLE);
+                tvSecond.setVisibility(View.INVISIBLE);
+                edtHour.setVisibility(View.INVISIBLE);
+                edtMinute.setVisibility(View.INVISIBLE);
+                edtSecond.setVisibility(View.INVISIBLE);
+                btnSchedule.setVisibility(View.INVISIBLE);
+
+                timer.scheduleAtFixedRate(new TimerTask(){
+                    @Override
+                    public void run(){
+                        Log.e("MainActivity", "Timer");
+                        //waterButtonClicked();
+                    }
+                },0,interval);
+            }
+        });
+
+        btnCancelSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.cancel();
+                Log.e("MainActivity", "Timer Cancelled");
+                btnCancelSchedule.setVisibility(View.INVISIBLE);
+            }
+        });
 
     }
 
@@ -100,6 +178,14 @@ public class MainActivity extends AppCompatActivity {
 
     //function to schedule waterings
     public void scheduleButtonClicked(){
+        tvHour.setVisibility(View.VISIBLE);
+        tvMinute.setVisibility(View.VISIBLE);
+        tvSecond.setVisibility(View.VISIBLE);
+        edtHour.setVisibility(View.VISIBLE);
+        edtMinute.setVisibility(View.VISIBLE);
+        edtSecond.setVisibility(View.VISIBLE);
+        btnSchedule.setVisibility(View.VISIBLE);
+        
         Log.d("scheduleBtn", "clicked" );
     }
 }
