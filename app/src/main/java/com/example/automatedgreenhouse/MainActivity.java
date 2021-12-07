e95eefad9dffb45158dfb1ba043a5f8b328d388d
shonenjunk <zachtyler8@gmail.com>
2021-12-07 16:22:48 -0600

    Automated Greenhouse with water scheduler and light control functionality

--- app/src/main/java/com/example/automatedgreenhouse/MainActivity.java
+++ app/src/main/java/com/example/automatedgreenhouse/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.automatedgreenhouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,6 +21,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Minimum moisture value allowed before the automatic water feature activates
+    public static final Long MIN_MOISTURE = Long.valueOf(500 / 10);
+    public static final Long MAX_MOISTURE = Long.valueOf(650 / 10);
+
+    public static Boolean lightBtnClicked = false;
 
     private TextView tvSunlight;
     private TextView tvMoisture;
@@ -42,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton imgBtnWater;
     private ImageButton imgBtnSchedule;
+    private ImageButton imgBtnLight;
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
@@ -69,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
         imgBtnWater = (ImageButton) findViewById(R.id.imgBtnWater);
         imgBtnSchedule = (ImageButton) findViewById(R.id.imgBtnSchedule);
+        imgBtnLight = (ImageButton) findViewById(R.id.imgBtnLight);
 
         tvHour.setVisibility(View.INVISIBLE);
         tvMinute.setVisibility(View.INVISIBLE);
@@ -95,10 +105,20 @@ public class MainActivity extends AppCompatActivity {
                 String moisture = moistureLevel.toString() + "%";
                 String temperature = dataSnapshot.child("temperature").getValue(Long.class).toString() + "°C";
                 String sunlight = dataSnapshot.child("sunlight").getValue(Long.class).toString() + "%";
-
-                tvMoisture.setText(moisture); //output the values from the database with units added in.
+                Boolean waterFlag = dataSnapshot.child("water").getValue(Boolean.class);
+                //output the values from the database with units added in.
+                tvMoisture.setText(moisture);
                 tvTemperature.setText(temperature);
                 tvSunlight.setText(sunlight);
+
+                //if the moisture gets below a certain threshold, then check if water boolean is false, then automatically tell the pi to water the plants.
+                if( (moistureLevel.compareTo(MIN_MOISTURE) < 0) && !waterFlag){
+                    waterButtonClicked();
+                }
+                else if ( (moistureLevel.compareTo(MAX_MOISTURE) > 0)  && waterFlag){ //if moisture is greater than the maximum moisture and water boolean is true, then set it to false
+                    turnWaterOff();
+                }
+
 
                 Log.d("Moisture: ", moisture);
                 Log.d("Sunlight: ", sunlight);
@@ -112,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //set on click listener for the water button button
         imgBtnWater.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -120,11 +141,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //set on click listener for the scheduler button
         imgBtnSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //call the schedule button clicked function to set watering schedule
                 scheduleButtonClicked();
+            }
+        });
+
+        //set on click listener for the light bulb button
+        imgBtnLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //call the light button clicked to set light boolean to true
+                lightButtonClicked();
             }
         });
 
@@ -170,6 +201,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
 
@@ -183,6 +215,28 @@ public class MainActivity extends AppCompatActivity {
         myRef.setValue(true);
         Log.d("WaterBtn", "clicked");
     }
+
+    //function to update the water_flag to be false
+    public void turnWaterOff(){
+        DatabaseReference myRef = database.getReference("Greenhouse").child("water");
+        myRef.setValue(false);
+    }
+
+    //function to turn the light on
+    public void lightButtonClicked(){
+        DatabaseReference myRef = database.getReference("Greenhouse").child("light");
+        if(lightBtnClicked){
+            myRef.setValue(false);
+            lightBtnClicked = false;
+        }
+        else{
+            myRef.setValue(true);
+            lightBtnClicked = true;
+        }
+
+        //myRef.setValue(true);
+    }
+
 
     //function to schedule waterings
     public void scheduleButtonClicked(){
